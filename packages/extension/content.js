16d06ba1ddf157db181d918f4afe8fb8cc05efe3
Benrobo <alumonabenaiah71@gmail.com>
2023-09-28 18:03:57 +0100

    added toggling of iframe

--- packages/extension/content.js
+++ packages/extension/content.js
@@ -2,15 +2,23 @@ console.log("Content script is running.");
 chrome.runtime.onMessage.addListener(function (msg, sender) {
   if (msg == "open_recorder") {
     console.log("MESSAGE RECEIVED");
-    insertIframe();
+    toggleScreenRecord();
   }
+});
+
+const $ = (elm) => document.querySelector(elm);
+const $all = (elm) => document.querySelectorAll(elm);
+
+window.addEventListener("DOMContentLoaded", () => {
+  insertIframe();
 });
 
 function insertIframe() {
   const iframe = document.createElement("iframe");
   const mainDiv = document.createElement("div");
   const url = chrome.runtime.getURL("index.html");
-  mainDiv.setAttribute("class", "help-me-iframe show");
+
+  mainDiv.setAttribute("class", "help-me-iframe-container hide");
   iframe.setAttribute("allowtransparency", "true");
   iframe.width = window.innerWidth;
   iframe.height = window.innerHeight;
@@ -19,4 +27,15 @@ function insertIframe() {
 
   mainDiv.append(iframe);
   document.body.appendChild(mainDiv);
+}
+
+function toggleScreenRecord() {
+  const mainDiv = $(".help-me-iframe-container");
+  if (mainDiv.classList.contains("show")) {
+    mainDiv.classList.remove("show");
+    mainDiv.classList.add("hide");
+  } else {
+    mainDiv.classList.remove("hide");
+    mainDiv.classList.add("show");
+  }
 }
