0eec5984cac5074a2923418bc030d8f6b2f52c4b
Benrobo <alumonabenaiah71@gmail.com>
2023-10-01 11:58:52 +0100

    added page to list all videos

--- packages/web/components/VideoCard.jsx
+++ packages/web/components/VideoCard.jsx
@@ -4,13 +4,14 @@ import React from "react";
 function VideoCard({ url, id, date }) {
   return (
     <Link
-      href={`#`}
+      href={`/file/${id}`}
       className="w-full p-4 max-w-[300px] h-[200px] border-solid border-white-300 border-[2px] flex flex-col items-start justify-start rounded-[10px] "
     >
       <video
         src={url}
         autoPlay
         muted
+        loop={true}
         className="w-full h-[120px] rounded-[10px] bg-white-300 object-cover"
       ></video>
       <p className="text-dark-100 font-ppSB text-[15px] mt-1">{id ?? "N/A"}</p>
