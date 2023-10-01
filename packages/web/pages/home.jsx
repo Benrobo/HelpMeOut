c79cd3871c4487debae9da7e0a31328e9004682b
Benrobo <alumonabenaiah71@gmail.com>
2023-10-01 06:19:24 +0100

    added major changes

--- packages/web/pages/home.jsx
+++ packages/web/pages/home.jsx
@@ -6,9 +6,16 @@ import Image from "next/image";
 function Home() {
   return (
     <Layout showFooter showTopBar={false}>
-      <div className="w-full flex items-center justify-between">
+      <div className="w-full flex flex-col items-center justify-between">
         <div className="w-full px-[4em] py-9 flex items-center justify-between">
           <Image src={Logo2} className="w-[100px] " />
+          <button className="px-3 flex items-center justify-center gap-2"></button>
+        </div>
+        <div className="w-full px-[4em] py-9 flex flex-col items-start justify-start">
+          <h1 className="text-dark-100 ppB text-[2em] ">Hello, John Mark</h1>
+          <p className="text-white-400 text-[12px] ppReg ">
+            Here are your recorded videos
+          </p>
         </div>
       </div>
     </Layout>
